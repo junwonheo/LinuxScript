62773d190d54ed2945f17038b76502f600306b9c
jwcs <jwcs1768@gmail.com>
2024-08-28 22:09:58 +0900

    complete

--- bob_homework.sh
+++ bob_homework.sh
@@ -38,6 +38,16 @@ fi
 echo "/etc/passwd: $(cat /etc/passwd)"
 }
 
+U-05(){
+if [ $(echo $PATH | grep -E '\.') ]; then
+    echo "U-05: Bad"
+else
+    echo "U-05: Good"
+fi
+
+echo "$(echo $PATH)"
+}
+
 U-06(){
 if [find / \( -nouser -o -nogroup \) -print -quit 2>/dev/null | grep -q .]; then
     echo "U-06: Bad"
@@ -60,16 +70,65 @@ echo "auth: $(stat -c %a /etc/passwd)"
 
 U-08(){
 if [ "$(stat -c %U /etc/shadow)" = "root" ] && [ "$(stat -c %a /etc/shadow)" -le 400 ]; then
-    echo "U-07: Good"
+    echo "U-08: Good"
 else
-    echo "U-07: Bad"
+    echo "U-08: Bad"
 fi
 echo "owner: $(stat -c %U /etc/shadow)"
 echo "auth: $(stat -c %a /etc/shadow)"
 }
 
-#U-02
-#U-04
-#U-06
-#U-07
+U-09(){
+if [ "$(stat -c %U /etc/hosts)" = "root" ] && [ "$(stat -c %a /etc/hosts)" -le 600 ]; then
+    echo "U-09: Good"
+else
+    echo "U-09: Bad"
+fi
+echo "owner: $(stat -c %U /etc/hosts)"
+echo "auth: $(stat -c %a /etc/hosts)"
+}
+
+U-11(){
+if [ "$(stat -c %U /etc/rsyslog.conf)" = "root" ] && [ "$(stat -c %a /etc/rsyslog.conf)" -le 600 ]; then
+    echo "U-11: Good"
+else
+    echo "U-11: Bad"
+fi
+echo "owner: $(stat -c %U /etc/rsyslog.conf)"
+echo "auth: $(stat -c %a /etc/rsyslog.conf)"
+}
+
+
+U-12(){
+if [ "$(stat -c %U /etc/services)" = "root" ] && [ "$(stat -c %a /etc/services)" -le 600 ]; then
+    echo "U-12: Good"
+else
+    echo "U-12: Bad"
+fi
+echo "owner: $(stat -c %U /etc/services)"
+echo "auth: $(stat -c %a /etc/services)"
+}
+
+U-20(){
+if [ !$(cat /etc/passwd | grep "ftp") ]; then
+    echo "U-20: Good"
+else
+    echo "U-20: Bad"
+fi
+echo "FTP User: $(cat /etc/passwd | grep 'ftp')"
+}
+
+start(){
+U-02
+U-04
+U-05
+U-06
+U-07
 U-08
+U-09
+U-11
+U-12
+U-20
+}
+
+start
